830f050159cd32b8ed604858b0b1dfe87153788f
gloria kitili <gloria.kitili@student.moringaschool.com>
2022-12-14 22:21:39 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complementaryNumber = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complementaryNumber) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
+
+
+
 
 /* 
   Write the Big O time complexity of your function here
-*/
+ loop 1 = n steps  
+  loop 2 (nested ) = n*n steps
+  Time complexity = O(n^2)
+  Space complexity = O(n)
 
 /* 
   Add your pseudocode here
+Add your pseudocode here
+  hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+  iterate through each number in the array 
+  for the correct number identify a number that adds to the target
+  the correct number is target - number  
+
 */
 
 /*
   Add written explanation of your solution here
+  Had to check whether any 2 numbers in the array add up to the target. 
+If an array includes [4,5,6,7] and the target is 11, it should return true. Otherwise it should return false
+Created a function hasTargetSum that take in an array and a target
+Wrote the first iteration of the array using it
 */
 
 // You can run `node index.js` to view these console logs
